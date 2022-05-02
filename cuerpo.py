0f92d37fe2b320943499ead6a58c413573e315ec
Tortrixxgd <zamoradani087@gmail.com>
2022-05-02 00:30:23 -0600

    numeros random

--- cuerpo.py
+++ cuerpo.py
@@ -1,3 +1,4 @@
+import random
 class Cuerpo():
     def __init__(self):
         self.cuerpo_s = []
@@ -16,6 +17,17 @@ class Cuerpo():
     def recorrer_sudoku(self):
         for filas in self.cuerpo_s:
             print(filas)
+    
+    #def posiciones(self): 
+        #self.posy = random.randrange(0,9,1)
+        #self.posx = random.randrange(0,9,1)
+
+    def colocandonumeros(self):
+        for i in range(1,5):
+            self.posy = random.randrange(0,9,1)
+            self.posx = random.randrange(0,9,1)
+            self.numerorandom = random.randrange(1,10,1)
+            self.cuerpo_s[self.posy][self.posx] = self.numerorandom
 
 
 
---
+++ main.py
@@ -0,0 +1,11 @@
+import cuerpo
+
+
+cuerpo_sudoku = cuerpo.Cuerpo()
+
+
+
+cuerpo_sudoku.iniciar_cuerpo()
+#cuerpo_sudoku.posiciones()
+cuerpo_sudoku.colocandonumeros()
+cuerpo_sudoku.recorrer_sudoku()
